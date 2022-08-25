629436e7a456cf759c046be18e3e4e1a09e2e953
enicio <jessandro42@gmail.com>
2022-08-25 14:31:33 -0300

    lint fix

--- src/bootstraps/common.ts
+++ src/bootstraps/common.ts
@@ -137,7 +137,7 @@ class KV {
         const response = await this.aws.fetch(url);
 
         if (response.status !== 200) {
-            const xmlContent = await  response.text();
+            const xmlContent = await response.text();
             const codeMatch = xmlContent.match(/<Code>(.+?)<\/Code>/);
             const messageMatch = xmlContent.match(/<Message>(.+?)<\/Message>/);
             console.log("Erro:", response.status,",", codeMatch[1],",", messageMatch[1]);
