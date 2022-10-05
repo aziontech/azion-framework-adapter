9511150fa595e9a518579c6109a94e83cd5ee227
enicio <jessandro42@gmail.com>
2022-10-05 14:55:05 -0300

    feat: add fake test file

---
+++ test.js
@@ -0,0 +1,24 @@
+async function run() {
+    // const util =  await import('node:util');
+    // eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+    // const exec = util.promisify(require('node:child_process').exec);
+     // const { stdout, stderr} = await exec('azion-framework-adapter --help');
+    // console.log(stdout);
+
+    const { spawn } = await import('node:child_process');
+    const afa =  spawn('azion-framework-adapter', ['--help']);
+
+    afa.stdout.on('data', (data) => {
+        console.log(`${data}`)
+    })
+
+    afa.stderr.on('data', (data) => {
+        console.log(`Saida padrão: ${data}`)
+    })
+
+    afa.on('close', (close) => {
+        console.log(`Close: ${close}`)
+    })
+}
+
+run();
