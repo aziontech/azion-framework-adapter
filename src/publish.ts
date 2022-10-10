20a3fd05a988f132cce0e19be287a867ca8f7d0a
enicio <jessandro42@gmail.com>
2022-10-10 20:59:47 -0300

    include endpoint paramater to S3 client config

--- src/publish.ts
+++ src/publish.ts
@@ -14,10 +14,15 @@ export async function publish(options: any): Promise<ErrorCode> {
 
         if (!options.onlyFunction) {
             const cfg: AssetPublisherConfig = await AssetPublisher.getConfig(rawCfg, process.env);
-            const s3 = new S3({
+            const s3 = new S3 ({
                 accessKeyId: cfg.kv.accessKeyId,
-                secretAccessKey: cfg.kv.secretAccessKey
+                secretAccessKey: cfg.kv.secretAccessKey,
+                signatureVersion: "v4",
+                s3ForcePathStyle: true
             });
+            if(cfg.kv.endpoint) {
+                s3.config.endpoint = cfg.kv.endpoint
+            }
             const publisher = new AssetPublisher(cwd(), s3, cfg);
             await publisher.deployStaticAssets(options.assetsDir);
         }
