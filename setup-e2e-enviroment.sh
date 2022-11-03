5888869e46d23a42393c2858cf343d396c9206fd
enicio <jessandro42@gmail.com>
2022-11-03 18:37:06 -0300

    test with htpasswd on storage

--- setup-e2e-enviroment.sh
+++ setup-e2e-enviroment.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
 cat ./verdaccio/config/config.yaml
-docker-compose up -d
+docker compose up -d
 docker exec -it verdaccio cat /verdaccio/conf/config.yaml
 npx json-server --port 8080 -w json-server-data/db.json -m json-server-data/file.js  -q &
