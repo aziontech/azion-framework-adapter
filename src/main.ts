13f3db35e447e85084bc5cc81e0779553a60210a
benjamim.neto <benjamim.neto@azion.com>
2023-02-17 08:31:12 -0300

    feat: applying version checker to verify node verison

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
+import { VersionChecker } from './utils/version-checker/version-checker';
 import { program } from 'commander';
 import { exit } from 'process';
 import { Builder } from './build';
 import * as init from './init';
 import { publish } from './publish';
+import { displayError } from "./errors";
 
 // Disabling the eslint rule is cleaner than other methods for embedding the
 // package version.
@@ -11,43 +13,50 @@ import { publish } from './publish';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { version } = require('../package.json');
 
-program.version(version);
+try{
+    program.version(version);
 
-program
-    .command('init')
-    .argument('[target-dir]','Target directory','.')
-    .argument('[repository]', 'Repository url')
-    .option('-n, --project-name <project-name>', 'Project name')
-    .option('-s, --static-site', 'Clone template to static site')
-    .description('Create a new project from a template.')
-    .action(async (targetDir, repository, options) => {
-        exit(await init.exec(targetDir, repository, options));
-    });
+    VersionChecker.node_version();
 
-program
-    .command('build')
-    .description('Build and upload.')
-    .option('-c, --config <config>', 'path to configuration file [default: azion.json]')
-    .option('-d, --assets-dir <directory>', 'path to static assets')
-    .option('-s, --static-site', 'build static site function')
-    .action(async (options) => {
-        exit(await Builder.exec(options));
-    });
+    program
+        .command('init')
+        .argument('[target-dir]','Target directory','.')
+        .argument('[repository]', 'Repository url')
+        .option('-n, --project-name <project-name>', 'Project name')
+        .option('-s, --static-site', 'Clone template to static site')
+        .description('Create a new project from a template.')
+        .action(async (targetDir, repository, options) => {
+            exit(await init.exec(targetDir, repository, options));
+        });
 
-program
-    .command('publish')
-    .description('Publish the application.')
-    .option('-c, --config <config>', 'path to configuration file [default: azion.json]')
-    .option('-d, --assets-dir <directory>', 'path to static assets')
-    .option('-e, --only-function', 'skip deploy of assets')
-    .option('-s, --only-assets', 'skip deploy of Edge Function')
-    .option('-t, --static-site', 'publish static site function')
-    .action(async (options) => {
-        if (options.onlyAssets && options.onlyFunction) {
-            console.warn("-e and -s must not be used at the same time");
-            exit(0);
-        }
-        exit(await publish(options));
-    });
+    program
+        .command('build')
+        .description('Build and upload.')
+        .option('-c, --config <config>', 'path to configuration file [default: azion.json]')
+        .option('-d, --assets-dir <directory>', 'path to static assets')
+        .option('-s, --static-site', 'build static site function')
+        .action(async (options) => {
+            exit(await Builder.exec(options));
+        });
 
-program.parse(process.argv);
+    program
+        .command('publish')
+        .description('Publish the application.')
+        .option('-c, --config <config>', 'path to configuration file [default: azion.json]')
+        .option('-d, --assets-dir <directory>', 'path to static assets')
+        .option('-e, --only-function', 'skip deploy of assets')
+        .option('-s, --only-assets', 'skip deploy of Edge Function')
+        .option('-t, --static-site', 'publish static site function')
+        .action(async (options) => {
+            if (options.onlyAssets && options.onlyFunction) {
+                console.warn("-e and -s must not be used at the same time");
+                exit(0);
+            }
+            exit(await publish(options));
+        });
+
+    program.parse(process.argv);
+
+}catch(error){
+    displayError(error);
+}
