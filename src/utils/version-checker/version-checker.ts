63e0b8b4c75aec6529995ac0fe684bf8ae0866c3
benjamim.neto <benjamim.neto@azion.com>
2023-02-20 15:27:53 -0300

    feat: coupling  nextjs version checker

--- src/utils/version-checker/version-checker.ts
+++ src/utils/version-checker/version-checker.ts
@@ -1,9 +1,20 @@
+import { NextJsChecker } from "./nextjs-checker/nextjs-checker";
 import { NodeChecker } from "./node-checker/node-checker";
+import * as fs from "fs";
+
 
 export class VersionChecker{
 
     public static node_version():boolean {
         return NodeChecker.check();
+    }
+
+    public static nextjs_version(target_dir:string):boolean {
+        
+        const package_json = JSON.parse(
+            fs.readFileSync(`${target_dir}/package.json`, "utf-8")
+        );
+        return NextJsChecker.check(package_json);
     }
 
 }
