7f1380312c74ff186e80b1e908370f890df4c954
benjamim.neto <benjamim.neto@azion.com>
2023-02-23 17:02:54 -0300

    refactor: parametizing erros to global error enum

--- src/utils/version-checker/errors/errors.ts
+++ src/utils/version-checker/errors/errors.ts
@@ -1,6 +1,12 @@
-export class InvalidProject extends Error {
+import { ErrorCode, BaseError } from "../../../errors";
+
+export class InvalidProject extends BaseError {
     constructor(message:string) {
         super(message);
         this.name = "InvalidProject";
+    }
+    
+    get errorCode(): ErrorCode {
+        return ErrorCode.InvalidProject;
     }
 }
--- src/utils/version-checker/nextjs-checker/errors/errors.ts
+++ src/utils/version-checker/nextjs-checker/errors/errors.ts
@@ -1,6 +1,12 @@
-export class InvalidNextJsVersion extends Error {
+import { ErrorCode, BaseError } from "../../../../errors";
+
+export class InvalidNextJsVersion extends BaseError {
     constructor(message:string) {
         super(message);
         this.name = "InvalidVersion";
+    }
+
+    get errorCode(): ErrorCode {
+        return ErrorCode.InvalidNextJsVersion;
     }
 }
