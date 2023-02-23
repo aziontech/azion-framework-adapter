d2db9b7f9719fceca94361482346ecd6b29f01a6
enicio <jessandro42@gmail.com>
2023-02-23 12:27:04 -0300

    Init cells site template with branch use-cells-fetch-api

--- test/e2e-test/nextjs.test.ts
+++ test/e2e-test/nextjs.test.ts
@@ -12,7 +12,7 @@ import AWS = require('aws-sdk');
 import { expect } from 'chai';
 import { CELLS_SITE_TEMPLATE_WORK_DIR } from '../../dist/constants';
 
-describe('Create nextjs application', () => {
+describe.only('Create nextjs application', () => {
     let templatePath: string;
     let template: string;
     let realPath: string;
@@ -89,12 +89,15 @@ describe('Create nextjs application', () => {
 
     it('Build the nextjs project', async () => {
         const configPath = path.join(template, CELLS_SITE_TEMPLATE_WORK_DIR, 'azion.json');
+        const functionPath = path.join(template, CELLS_SITE_TEMPLATE_WORK_DIR, 'worker', 'function.js')
         await copy(path.join(localOutput, 'test', 'config-files', 'azion-next.json'), configPath)
         const expectOutput = `Static site template initialized. Building ...\n`+
         `Finished worker.\n`+
         `Completed.\n`
-        const { stdout } = await execFile(`azion-framework-adapter build -c ${configPath} --static-site --assets-dir ./out || exit $? | 2>&1`);
-        const functionFile = fs.existsSync(path.join(template, CELLS_SITE_TEMPLATE_WORK_DIR, 'worker', 'function.js'));
+        const { stdout } = await execFile(`azion-framework-adapter build -c ${configPath} --version-id k0mb1 --static-site --assets-dir ./out || exit $? | 2>&1`);
+        const functionContent = fs.readFileSync(functionPath, 'utf8');
+        const functionFile = fs.existsSync(functionPath);
+        expect(functionContent).to.include('k0mb1');
         expect(stdout).to.be.equal(expectOutput);
         expect(functionFile).to.be.true;
     });
