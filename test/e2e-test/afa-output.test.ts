e0aac166cf36a65830d979501141688ebd650c6b
enicio <jessandro42@gmail.com>
2023-02-23 09:13:22 -0300

    Removed version-id option because it was add in another PR

--- test/e2e-test/afa-output.test.ts
+++ test/e2e-test/afa-output.test.ts
@@ -36,7 +36,6 @@ Options:
 Build and upload.
 
 Options:
-  -v, --version-id <versionid>  build static site function
   -c, --config <config>         path to configuration file [default:
                                 azion.json]
   -d, --assets-dir <directory>  path to static assets
