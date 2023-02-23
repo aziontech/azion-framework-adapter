f1680fff86c939f25fc3ed62392e35f5f89fd19f
benjamim.neto <benjamim.neto@azion.com>
2023-02-23 11:14:03 -0300

    test: testing error scenarios

--- test/ts/utils/version-checker/nextjs-checker/nextjs-checker.test.ts
+++ test/ts/utils/version-checker/nextjs-checker/nextjs-checker.test.ts
@@ -1,30 +1,47 @@
+import { InvalidNextJsVersion } from  "../../../../../dist/utils/version-checker/nextjs-checker/errors/errors";
 import { NextJsChecker} from "../../../../../dist/utils/version-checker/nextjs-checker/nextjs-checker";
 import { expect } from "chai";
 
+// eslint-disable-next-line prefer-const
+let package_mock = {
+    dependencies: {
+        ms: '2.1.3',
+        next: '12.2.1',
+        react: '^18.1.0',
+        'react-dom': '^18.1.0'
+    }
+}
 
 describe("nextjs checker",()=>{
     it(" Should call check method with success",()=>{
-        const package_mock = {
-            scripts: {
-                dev: 'yarn next dev',
-                build: 'yarn next build',
-                start: 'yarn next start'
-            },
-            dependencies: {
-                ms: '2.1.3',
-                next: '12.2.1',
-                react: '^18.1.0',
-                'react-dom': '^18.1.0'
-            },
-            devDependencies: {
-                '@netlify/build': '^27.20.6',
-                '@netlify/functions': '^1.3.0',
-                '@netlify/plugin-nextjs': '^4.24.3',
-                '@types/fs-extra': '^9.0.13',
-                'fs-extra': '^10.0.1',
-                vercel: '^28.4.17'
-            }
-        }
         expect(NextJsChecker.check(package_mock)).equal(true);
-    })
+    });
+
+    it(" Should return a InvalidNextjsVersion error instance if a latest version have been setted",()=>{
+        package_mock.dependencies.next = "latest";
+        try{
+            NextJsChecker.check(package_mock)
+        }catch(error){
+            expect(error).instanceOf(InvalidNextJsVersion);
+        }
+    });
+
+    it(" Should return a InvalidNextjsVersion error instance if a different version of 12.XX have been setted",()=>{
+        package_mock.dependencies.next = "13.0.1";
+        try{
+            NextJsChecker.check(package_mock)
+        }catch(error){
+            expect(error).instanceOf(InvalidNextJsVersion);
+        }
+    });
+
+    it(" Should return a InvalidNextjsVersion error instance if a canary version have been setted",()=>{
+        package_mock.dependencies.next = "12.3.2-canary.0";
+        try{
+            NextJsChecker.check(package_mock)
+        }catch(error){
+            expect(error).instanceOf(InvalidNextJsVersion);
+        }
+    });
+
 });
