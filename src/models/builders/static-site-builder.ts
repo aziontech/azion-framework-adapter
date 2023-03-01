f9d60538c92e1019a49a317a82c16e462337fd3e
enicio <jessandro42@gmail.com>
2023-03-01 18:09:31 -0300

    Add static build

--- src/models/builders/static-site-builder.ts
+++ src/models/builders/static-site-builder.ts
@@ -1,43 +1,80 @@
 import { Builder } from "./builder";
-import { ErrorCode } from '../../errors';
+import { displayError, errorCode } from '../../errors';
 
 import * as fs from 'fs';
 import { build } from 'esbuild';
 import { entrypoint } from "../../entrypoint/entrypoints.js";
 
-const ERROR_MESSAGE = "Static Site build not implemented!";
+import { spawn } from "child_process";
 
 class StaticSiteBuilder extends Builder {
     constructor(targetDir: string) {
         super(targetDir);
     }
 
-    async build(params: any): Promise<ErrorCode> {
-        console.log(params);
+    async build(_params: any): Promise<any> {
 
-        throw Error(ERROR_MESSAGE);
+        const outputLogs = (data: any) => {
+            const lines: string[] = data.toString().split("\n");
+            lines.map((line) => {
+                if (line.length > 0) console.log(`▲ ${line}`);
+            });
+        };
+
+        const nextBuild = spawn("npx", ["next", "build"]);
+
+        nextBuild.stdout.on("data", (data) => outputLogs(data));
+
+        nextBuild.stderr.on("data", (data) => outputLogs(data));
+
+        await new Promise((resolve, reject) => {
+            nextBuild.on("close", (code) => {
+                if (code === 0) {
+                    resolve(null);
+                } else {
+                    reject();
+                }
+            });
+        });
+
+        const nextExport = spawn("npx", ["next", "export"]);
+
+        nextExport.stdout.on("data", (data) => outputLogs(data));
+
+        nextExport.stderr.on("data", (data) => outputLogs(data));
+
+        await new Promise((resolve, reject) => {
+            nextExport.on("close", (code) => {
+                if (code === 0) {
+                    resolve(null);
+                } else {
+                    reject();
+                }
+            });
+        });
     }
 
     async buildWorker(params: any): Promise<any> {
-        console.log(params);
-        console.log("Coping entrypoint file");
-        fs.mkdirSync('azion',{recursive: true});
-        fs.writeFileSync('azion/index.js', entrypoint.defaultEntrypoint)
+        try {
+            fs.mkdirSync('azion/worker',{recursive: true});
+            fs.writeFileSync('azion/index.js', entrypoint.defaultEntrypoint);
+        } catch(e) {
+            displayError(e);
+            return errorCode(e);
+        }
 
-        console.log("esBuild Function");
+        console.log("Initialising build.");
         await build({
             entryPoints: ['azion/index.js'],
             bundle: true ,
             define: {
                 self: 'globalThis',
-                VERSION_ID: JSON.stringify(params.versionId),
-                PROJECT_TYPE_PATTERN_VALUE: JSON.stringify("PROJECT_TYPE:STATIC_SITE")
+                VERSION_ID: JSON.stringify(params.versionId)
             },
-            inject: ['./bootstrap.js'],
             sourcemap: false,
             target: "esnext",
             minify: false,
-            outdir: 'azion/worker',
+            outfile: 'azion/worker/function.js'
         }).catch((e) => {
             console.log("Error", e)
             process.exit(1)
