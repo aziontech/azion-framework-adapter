d65e432a46109646728a1d123ef3155ffd9e7b65
enicio <jessandro42@gmail.com>
2023-03-15 17:24:23 -0300

    Removed unnecessary constant entrypoint

--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,9 @@
 const CELLS_SITE_TEMPLATE_REPO = "https://github.com/aziontech/cells-site-template.git";
 const CELLS_SITE_TEMPLATE_WORK_DIR = "azion/cells-site-template";
 const WORKER_DIR = "azion/worker";
-const ENTRYPOINT =  "azion/index.js";
 
 export {
     CELLS_SITE_TEMPLATE_REPO,
     CELLS_SITE_TEMPLATE_WORK_DIR,
-    WORKER_DIR,
-    ENTRYPOINT
+    WORKER_DIR
 }
