14daa952d75beb3d53cf214d36ba3be1188f17e4
enicio <jessandro42@gmail.com>
2023-03-15 17:45:23 -0300

    Remove bootstraputilss

--- src/models/builders/static-site-builder.ts
+++ src/models/builders/static-site-builder.ts
@@ -1,54 +1,28 @@
 import { Builder } from "./builder";
-import { displayError, errorCode } from '../../errors';
-
-import path from "path";
-import * as fs from 'fs';
+import { join,  dirname } from "path";
 import * as esbuild from 'esbuild';
 
-import { ENTRYPOINT, WORKER_DIR } from "../../constants";
-import { entrypoint } from "../../entrypoint/entrypoints.js";
-import { BootstrapUtils } from "../../bootstraps/utils";
+import { WORKER_DIR } from "../../constants";
 
 class StaticSiteBuilder extends Builder {
-    private entrypointPath: string;
     private outputPath: string;
     private outfile: string;
+    private dirname: string = dirname(__filename);
     constructor(targetDir: string) {
         super(targetDir);
-        this.entrypointPath = path.join(this.targetDir,ENTRYPOINT);
-        this.outputPath = path.join(this.targetDir,WORKER_DIR);
-        this.outfile = path.join(this.outputPath,'function.js');
+        this.outputPath = join(this.targetDir,WORKER_DIR);
+        this.outfile = join(this.outputPath,'function.js');
     }
 
     async build(params: any): Promise<any> {
         this.createWorkerDir();
-        this.createEntrypointFunction();
         await this.buildWorker(params);
-    }
-
-    createEntrypointFunction() {
-        const bootstrapCode = ' self.__PROJECT_TYPE_PATTERN = PROJECT_TYPE_PATTERN_VALUE;';
-
-        try {
-            fs.mkdirSync(this.outputPath,{recursive: true});
-            fs.writeFileSync(this.entrypointPath, entrypoint.defaultEntrypoint.toString() );
-
-
-            const bootstrapUtils = new BootstrapUtils(
-                this.entrypointPath,
-                bootstrapCode
-            );
-            bootstrapUtils.addBootstrap();
-        } catch(e) {
-            displayError(e);
-            return errorCode(e);
-        }
     }
 
     async buildWorker(params: any): Promise<any> {
         console.log("Initialising build.");
         await esbuild.build({
-            entryPoints: [this.entrypointPath],
+            entryPoints: [join(this.dirname, '../../entrypoint/entrypoints-2.js')],
             bundle: true ,
             define: {
                 self: 'globalThis',
@@ -58,15 +32,13 @@ class StaticSiteBuilder extends Builder {
             sourcemap: false,
             target: "es2021",
             platform: "neutral",
-            minify: false,
+            minify: true,
             outfile: this.outfile
         }).catch((e) => {
             console.log("Error", e)
             process.exit(1)
         }
-        ).finally(() => {
-            fs.rmSync(this.entrypointPath,{recursive: true});
-        });
+        );
     }
 }
 
