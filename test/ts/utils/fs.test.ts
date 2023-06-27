843db6289ea0fcfc076313853b30259e2bafa0de
benjamim <benjamim.neto@azion.com>
2023-06-27 10:45:26 -0300

    test: reandJsonFile test suite

--- test/ts/utils/fs.test.ts
+++ test/ts/utils/fs.test.ts
@@ -1,4 +1,5 @@
 import * as chai from 'chai';
+import * as fs from 'fs/promises';
 
 import {
 	copyFileWithDir,
@@ -8,10 +9,10 @@ import {
 	validateDir,
 	validateFile,
 } from '../../../dist/utils/fs';
-
+import mockFs = require('mock-fs');
 const { expect } = chai;
 
-describe.only('normalizePath', () => {
+describe('normalizePath', () => {
 	it('windows short path name format normalizes', () => {
 		const path = 'D:\\very short path';
 		const expected = 'D:/very short path';
@@ -29,5 +30,39 @@ describe.only('normalizePath', () => {
 		const path = '\\\\?\\D:\\very long path';
 
 		expect(normalizePath(path)).to.equal(path);
+	});
+});
+
+describe('readJsonFile', () => {
+    afterEach(()=>{
+        chai.spy.restore();
+    });
+    
+	it('should read a valid JSON file', async () => {
+		const vcConfigContent = {
+			runtime: 'edge',
+			entrypoint: 'index.js',
+		};
+        chai.spy.on(fs,'readFile',(path,encoding)=>{
+            return JSON.stringify(vcConfigContent)
+        });
+
+        const result = await (async()=>{
+            try{
+                return await readJsonFile('.vc-config.json') 
+            }catch(error:any){
+                return error.message
+            }
+        })();
+		expect(result.runtime).to.equal('edge');
+        expect(result.entrypoint).to.equal('index.js');
+	});
+
+	it('should return null with invalid json file', async () => {
+		mockFs({
+			'invalid.json': 'invalid-file',
+		});
+		expect(await readJsonFile('invalid.json')).to.equal(null);
+		mockFs.restore();
 	});
 });
