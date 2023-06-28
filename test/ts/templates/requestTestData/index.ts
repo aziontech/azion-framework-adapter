886e2df92c56aa6a0fb3fcfeb978e4fde3fe0ccd
enicio <jessandro42@gmail.com>
2023-06-28 10:12:52 -0300

    Remove requestTestData dir

--- test/ts/templates/requestTestData/index.ts
+++
@@ -1,9 +0,0 @@
-export { testSet as basicEdgeAppDirTestSet } from './basicEdgeAppDir';
-export { testSet as basicStaticAppDirTestSet } from './basicStaticAppDir';
-export { testSet as checkRouteMatchTestSet } from './checkRouteMatch';
-export { testSet as configRewritesRedirectsHeadersTestSet } from './configRewritesRedirectsHeaders';
-export { testSet as dynamicRoutesTestSet } from './dynamicRoutes';
-export { testSet as i18nTestSet } from './i18n';
-export { testSet as infiniteLoopTestSet } from './infiniteLoop';
-export { testSet as middlewareTestSet } from './middleware';
-export { testSet as trailingSlashTestSet } from './trailingSlash';
