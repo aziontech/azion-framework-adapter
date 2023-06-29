c2db42eedbf4fe7bfaff69471fa2a5c01bccc809
enicio <jessandro42@gmail.com>
2023-06-29 20:18:30 +0000

    Change tminify to true

--- src/models/builders/nextjs-builder.ts
+++ src/models/builders/nextjs-builder.ts
@@ -92,7 +92,7 @@ class NextjsBuilder extends Builder {
                     params.outputReferencesFilePath,
                     join(this.dirname, "../../templates/handlers/nextjs/globals.js"),
                 ],
-                minify: false,
+                minify: true,
                 target: "es2022",
                 platform: "neutral",
                 define: {
